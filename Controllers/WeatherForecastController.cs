190194eea5ba4e266a08d6c1a752a5c2ca278400
Cristian <cx.chuquimarca@gmail.com>
2022-06-04 19:38:55 -0500

    una version mas

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
     {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","Frio"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","Frio5555"
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
